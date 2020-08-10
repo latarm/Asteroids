26f787a54d8fd42288bc10fb84118c67f1518c13
latarm <titonko@list.ru>
2020-08-10 08:40:36 +0300

    Some thrash

---
+++ Assets/Scripts/SO/GameData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="New game data", menuName = "Game data", order =1)]
+public class GameData : ScriptableObject
+{
+    public LevelData[] LevelDatas;
+    public ShipData[] ShipDatas;
+}
